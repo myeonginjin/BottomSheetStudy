57aac34c023294039f4cc4bceaf0cf35bb9e4afc
myeongin <myeongin@jellybus.com>
2024-03-25 18:35:02 +0900

    [main] 커스텀 바텀시트 초안 구현

--- BottomSheetStudy/BottomSheetViewController.swift
+++ BottomSheetStudy/BottomSheetViewController.swift
@@ -6,3 +6,286 @@
 //
 
 import Foundation
+import UIKit
+
+class BottomSheetViewController: UIViewController {
+    
+    
+    
+    
+    //바텀 시트 팝업되어있는 정도 설정 (expanded상태의 snap효과 구현. 단, 현재 개발팀 기획의도와 다름)
+    enum BottomSheetViewState {
+        case expanded
+        case normal
+    }
+    
+    private let contentViewController: UIViewController
+    
+    // 이니셜라이저 구현
+    init(contentViewController: UIViewController) {
+        self.contentViewController = contentViewController
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    //바텀시트와 safe Area Top 사이의 마진 최소값
+    var bottomSheetPanMinTopConstant: CGFloat = 50.0
+    
+    
+    // BottomSheetViewController.swift 드래그 인디케이터
+    private let dragIndicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 3
+        return view
+    }()
+    
+    
+    private lazy var bottomSheetPanStartingTopConstant: CGFloat = bottomSheetPanMinTopConstant
+    
+    //바텀 시트 팝업됐을 때 기본 설정 마진 값
+    private var bottomSheetViewTopConstraint: NSLayoutConstraint!
+    var defaultHeight: CGFloat = 500
+    
+    
+    //플러스 버튼 클릭시 시작
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showBottomSheet()
+    }
+    
+    
+    private let dimmedView: UIView = {
+        let view = UIView()
+//        view.backgroundColor = UIColor.darkGray.withAlphaComponent(0.7)
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    
+    private let bottomSheetView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10
+        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    
+    override func viewDidLoad() {
+        view.addSubview(dimmedView)
+        view.addSubview(bottomSheetView)
+        view.addSubview(dragIndicatorView)
+        
+        //바텀시트 안 내용물 뷰로 채워줄 컨트롤러
+        addChild(contentViewController)
+        
+        bottomSheetView.addSubview(contentViewController.view)
+        contentViewController.didMove(toParent: self)
+        bottomSheetView.clipsToBounds = true
+        dimmedView.alpha = 0.0
+        
+        
+        
+        setUpLayout()
+        
+        let dimmedTap = UITapGestureRecognizer(target: self, action: #selector(dimmedViewTapped(_:)))
+        dimmedView.addGestureRecognizer(dimmedTap)
+        dimmedView.isUserInteractionEnabled = true
+        
+        
+        // Pan Gesture Recognizer를 view controller의 view에 추가하기 위한 코드
+        let viewPan = UIPanGestureRecognizer(target: self, action: #selector(viewPanned(_:)))
+        
+        // 기본적으로 iOS는 터치가 드래그하였을 때 딜레이가 발생함
+        // 우리는 드래그 제스쳐가 바로 발생하길 원하기 때문에 딜레이가 없도록 아래와 같이 설정
+        viewPan.delaysTouchesBegan = false
+        viewPan.delaysTouchesEnded = false
+        view.addGestureRecognizer(viewPan)
+        
+        
+    }
+    
+    private func setUpLayout(){
+        dimmedView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            dimmedView.topAnchor.constraint(equalTo: view.topAnchor),
+            dimmedView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            dimmedView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            dimmedView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        
+        bottomSheetView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let topConstant = view.safeAreaLayoutGuide.layoutFrame.height
+        
+        bottomSheetViewTopConstraint = bottomSheetView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstant)
+
+        
+        NSLayoutConstraint.activate([
+            bottomSheetViewTopConstraint,
+            bottomSheetView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomSheetView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomSheetView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        
+        dragIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            dragIndicatorView.widthAnchor.constraint(equalToConstant: 60),
+            dragIndicatorView.heightAnchor.constraint(equalToConstant: dragIndicatorView.layer.cornerRadius * 2),
+            dragIndicatorView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            dragIndicatorView.bottomAnchor.constraint(equalTo: bottomSheetView.topAnchor, constant: -10)
+        ])
+        
+        
+        contentViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentViewController.view.topAnchor.constraint(equalTo: bottomSheetView.topAnchor),
+            contentViewController.view.leadingAnchor.constraint(equalTo: bottomSheetView.leadingAnchor),
+            contentViewController.view.trailingAnchor.constraint(equalTo: bottomSheetView.trailingAnchor),
+            contentViewController.view.bottomAnchor.constraint(equalTo: bottomSheetView.bottomAnchor)
+        ])
+    }
+    
+    
+    private func showBottomSheet(atState: BottomSheetViewState = .normal) {
+        if atState == .normal {
+            let safeAreaHeight: CGFloat = view.safeAreaLayoutGuide.layoutFrame.height
+            let bottomPadding: CGFloat = view.safeAreaInsets.bottom
+            
+            
+            //플러스 버튼 클릭되었을 때 초기 높이값 설정
+            bottomSheetViewTopConstraint.constant = (safeAreaHeight + bottomPadding) - defaultHeight
+        } else {
+            
+            //바텀시트 드래그 통해 위로 올렸을 때 최종 높이값 설정
+            bottomSheetViewTopConstraint.constant = bottomSheetPanMinTopConstant
+        }
+        
+        //바텀 시트 팝업 정도에 따라 dimmedView의 알파값을 조정
+        UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseIn, animations: {
+                    // ------------ 수정된 코드
+                    self.dimmedView.alpha = self.dimAlphaWithBottomSheetTopConstraint(value: self.bottomSheetViewTopConstraint.constant)
+                    // ------------
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    @objc private func dimmedViewTapped(_ tapRecognizer: UITapGestureRecognizer) {
+        hideBottomSheetAndGoBack()
+    }
+    
+    private func hideBottomSheetAndGoBack() {
+        let safeAreaHeight = view.safeAreaLayoutGuide.layoutFrame.height
+        let bottomPadding = view.safeAreaInsets.bottom
+        bottomSheetViewTopConstraint.constant = safeAreaHeight + bottomPadding
+        UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseIn, animations: {
+            self.dimmedView.alpha = 0.0
+            self.view.layoutIfNeeded()
+        }) { _ in
+            if self.presentingViewController != nil {
+                self.dismiss(animated: false, completion: nil)
+            }
+        }
+    }
+    
+    // 해당 메소드는 사용자가 view를 드래그하면 실행됨
+    @objc private func viewPanned(_ panGestureRecognizer: UIPanGestureRecognizer) {
+        let translation = panGestureRecognizer.translation(in: view)
+        
+        let velocity = panGestureRecognizer.velocity(in: view)
+        
+        
+        switch panGestureRecognizer.state {
+        case .began:
+            bottomSheetPanStartingTopConstant = bottomSheetViewTopConstraint.constant
+        case .changed:
+            if bottomSheetPanStartingTopConstant + translation.y > bottomSheetPanMinTopConstant {
+                bottomSheetViewTopConstraint.constant = bottomSheetPanStartingTopConstant + translation.y
+            }
+            
+            //바텀시트 팝업 정도에 따라 dimmedView의 알파값을 조정해주는 메서드
+            dimmedView.alpha = dimAlphaWithBottomSheetTopConstraint(value: bottomSheetViewTopConstraint.constant)
+            
+        case .ended:
+            
+            //드래그 속도가 빠르다면 한번에 모달창이 내려가도록 함
+            if velocity.y > 3000 {
+                hideBottomSheetAndGoBack()
+                return
+            }
+            
+            let safeAreaHeight = view.safeAreaLayoutGuide.layoutFrame.height
+            let bottomPadding = view.safeAreaInsets.bottom
+            let defaultPadding = safeAreaHeight+bottomPadding - defaultHeight
+            
+            let nearestValue = nearest(to: bottomSheetViewTopConstraint.constant, inValues: [bottomSheetPanMinTopConstant, defaultPadding, safeAreaHeight + bottomPadding])
+            
+                    //
+            if nearestValue == bottomSheetPanMinTopConstant {
+                showBottomSheet(atState: .expanded)
+            } else if nearestValue == defaultPadding {
+                // Bottom Sheet을 .normal 상태로 보여주기
+                showBottomSheet(atState: .normal)
+            } else {
+                // Bottom Sheet을 숨기고 현재 View Controller를 dismiss시키기
+                hideBottomSheetAndGoBack()
+            }
+            
+            
+        default:
+            break
+        }
+    }
+    
+    //주어진 CGFloat 배열의 값 중 number로 주어진 값과 가까운 값을 찾아내는 메소드
+    func nearest(to number: CGFloat, inValues values: [CGFloat]) -> CGFloat {
+        guard let nearestVal = values.min(by: { abs(number - $0) < abs(number - $1) })
+        else { return number }
+        return nearestVal
+    }
+    
+    // Bottom시트뷰의 TopConstraint의 constant 값을 받아 dimmendView의 알파값을 계산하는 메소드
+    private func dimAlphaWithBottomSheetTopConstraint(value: CGFloat) -> CGFloat {
+        let fullDimAlpha: CGFloat = 0.7
+        
+        let safeAreaHeight = view.safeAreaLayoutGuide.layoutFrame.height
+        let bottomPadding = view.safeAreaInsets.bottom
+        
+            // bottom sheet의 top constraint 값이 fullDimPosition과 같으면
+            // dimmer view의 alpha 값이 0.7이 되도록 합니다
+        let fullDimPosition = (safeAreaHeight + bottomPadding - defaultHeight) / 2
+
+            // bottom sheet의 top constraint 값이 noDimPosition과 같으면
+            // dimmer view의 alpha 값이 0.0이 되도록 합니다
+        let noDimPosition = safeAreaHeight + bottomPadding
+
+            // Bottom Sheet의 top constraint 값이 fullDimPosition보다 작으면
+            // 배경색이 가장 진해진 상태로 지정해줍니다.
+        if value < fullDimPosition {
+            return fullDimAlpha
+        }
+        
+            // Bottom Sheet의 top constraint 값이 noDimPosition보다 크면
+            // 배경색이 투명한 상태로 지정해줍니다.
+        if value > noDimPosition {
+            return 0.0
+        }
+        
+            // 그 외의 경우 top constraint 값에 따라 0.0과 0.7 사이의 alpha 값이 Return되도록 합니다
+        return fullDimAlpha * (1 - ((value - fullDimPosition) / (noDimPosition - fullDimPosition)))
+    }
+    
+    
+
+    
+}
+
