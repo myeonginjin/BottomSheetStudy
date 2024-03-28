910eb9ae80000ced7c01f02619674befbe7fb9d5
myeongin <myeongin@jellybus.com>
2024-03-28 18:20:56 +0900

    [main] V1 주석 추가

--- BottomSheetStudy/AMHomeSheetControll.swift
+++ BottomSheetStudy/AMHomeSheetControll.swift
@@ -8,24 +8,40 @@
 import Foundation
 import UIKit
 
+enum SheetConstraint: CGFloat{
+    case minTopConstant = 20.0
+    case defaulTopConstant = 450.0
+    case minBottomConstant = 100.0
+}
 
 
-
-
+// 커스텀 바텀시트
 class AMHomeSheetControll: UIView{
 
     
+    //해당 프로토콜은 didScroll 메소드를 구현하도록(채택하도록) 요구하는 프로토콜. 준수해야할 클래스 타입은 AnyObject(어느 객체든 가능),
+    // didScroll 메소드는 AMHomeSheetControll객체가 UITapGestureRecognizer에 의해 클릭이 감지됐을 때 호출되는 handleTapGesture메소드에 의해 호출.
     weak var actionDelegate: AMHomeSheetControllDelegate?
     
     
-
+    //최대 확장, 기본 크기, 최소 확장을 만들어줄 오토레이아웃 마진값 변수들
+    var minTopConstant: SheetConstraint = .minTopConstant
+    var defaulTopConstant: SheetConstraint = .defaulTopConstant
+    var minBottomConstant: SheetConstraint = .minBottomConstant
+    
+    
+    // 바텀시트를 무조건적으로 올렷다 내렷다 할 수 있는 영역의 뷰
     let dragIndicatorView: UIView = {
         let view = UIView()
+        
+        //UI 디버깅 용 하드코딩
         view.backgroundColor = .blue
+        
         return view
     }()
     
-    let radiusBar: UIView = {
+    // dragIndicatorView 영역을 인지할 수 있도록 해주는 손잡이 부분
+    let handleBar: UIView = {
         let view = UIView()
         view.backgroundColor = .white
         view.layer.cornerRadius = 3
@@ -39,23 +55,28 @@ class AMHomeSheetControll: UIView{
         super.init(frame: .zero)
         self.backgroundColor = .brown
         
+        //모서리 곡률 반경 지정
         self.layer.cornerRadius = 10
+        //왼쪽 상단과 오른쪽 상단에만 적용
         self.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        //이 뷰에 붙을 서브뷰들이 경계를 벗어나 곡률 처리된 영역을 침범할 경우 잘라내도록 설정
         self.clipsToBounds = true
+        
         
         self.translatesAutoresizingMaskIntoConstraints = false
         
-        let Tap = UITapGestureRecognizer(target: self, action: #selector(Scroll))
         
-
-        
+        //UITapGestureRecognizer를 통해 해당 뷰 객체가 클릭이 인식될 경우 handleTapGesture메소드 호출하도록 선언
+        let Tap = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
         self.addGestureRecognizer(Tap)
         
+        
+        
         self.addSubview(dragIndicatorView)
-        dragIndicatorView.addSubview(radiusBar)
+        dragIndicatorView.addSubview(handleBar)
         
         dragIndicatorView.translatesAutoresizingMaskIntoConstraints = false
-        radiusBar.translatesAutoresizingMaskIntoConstraints = false
+        handleBar.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             dragIndicatorView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
@@ -63,10 +84,10 @@ class AMHomeSheetControll: UIView{
             dragIndicatorView.heightAnchor.constraint(equalToConstant: 30),
             dragIndicatorView.topAnchor.constraint(equalTo: self.topAnchor),
             
-            radiusBar.widthAnchor.constraint(equalToConstant: 60),
-            radiusBar.heightAnchor.constraint(equalToConstant: radiusBar.layer.cornerRadius * 2),
-            radiusBar.centerXAnchor.constraint(equalTo: dragIndicatorView.centerXAnchor),
-            radiusBar.centerYAnchor.constraint(equalTo: dragIndicatorView.centerYAnchor),
+            handleBar.widthAnchor.constraint(equalToConstant: 60),
+            handleBar.heightAnchor.constraint(equalToConstant: handleBar.layer.cornerRadius * 2),
+            handleBar.centerXAnchor.constraint(equalTo: dragIndicatorView.centerXAnchor),
+            handleBar.centerYAnchor.constraint(equalTo: dragIndicatorView.centerYAnchor),
         ])
         
 
@@ -74,12 +95,17 @@ class AMHomeSheetControll: UIView{
     
 
     
-    
+    //스위프트의 UIView 클래스를 상속받은 클래스가 인터페이스 빌더(IB)나 스토리보드에서 사용될 때 필요한 초기화 경로
     required public init?(coder: NSCoder) {
+        //fatalError("init(coder:)이 메서드가 호출되면 애플리케이션이 크래시되도록 강제. 해당 클래스의 인스턴스가 스토리보드 또는 XIB 파일을 통해 생성되는 것을 의도하지 않았음을 나타냄. 이 클래스는 코드를 통해서만 인스턴스화되어야 한다는 것을 의미.
+        //즉, 스토리보드나 XIB를 사용하여 인스턴스화되는 것을 원하지 않는 클래스에 대한 명확한 신호로 작용
+        //스토리보드나 XIB 파일을 사용하여 이 클래스의 인스턴스를 생성하려할 때 막는 기능도 있음
         fatalError("init(coder:) has not been implemented")
     }
     
-    @objc func Scroll() {
+    //UITapGestureRecognizer에 의해 제스처가 인식됐을 경우 호출되는 메서드
+    @objc func handleTapGesture() {
+        //델리게이트의 didScroll메서드를 호출하도록 구현
         actionDelegate?.didScroll()
     }
     
@@ -87,8 +113,7 @@ class AMHomeSheetControll: UIView{
 
 
 
-
-
+//해당 클래스의 인스턴스를 생성하고 싶다면 이 프로토콜을 준수해야됨. 이 프로토콜은 AnyObject타입이 채택할 수 있고, didScroll 메소드를 채택해 코드 몸체 부분을 구현해야됨.
 protocol AMHomeSheetControllDelegate: AnyObject {
     func didScroll()
 }
--- BottomSheetStudy/AMHomeViewController.swift
+++ BottomSheetStudy/AMHomeViewController.swift
@@ -19,26 +19,30 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
         //중간 턱
         case normal
         
-        //최대 축소 현재 (기획의도x)
-        case minimum
+        
+        case minimum //최대 축소 (현재 기획의도x)
     }
     
     
 
-    // 현재 바텀 시트의 상태를 추적하기 위한 변수 추가
+    // 현재 바텀 시트의 상태를 추적하기 위한 변수
     var currentSheetState: SheetViewState = .normal
 
     
 
-    // 드래그 하기 전에 Bottom Sheet의 top Constraint value를 저장하기 위한 프로퍼티
+    // 드래그 시작 점에서(화면터치) Bottom Sheet의 현재 top Constraint value를 저장하기 위한 프로퍼티
     private lazy var sheetPanStartingTopConstant: CGFloat = sheetPanMinTopConstant
     
     
-    
+    //뷰컨트롤러 view에 붙어있을 버튼
     var testBtn: UIButton?
+    //바텀시트
     var sheetControll: AMHomeSheetControll?
+    //바텀시트 안 내용물(content) 뷰
     var contentSheetItemView: AMHomeContentSheetItemView?
     
+    
+    //바텀시트의 크기를 동적으로(제스처에 따라 변화도록) 변하시키기 위해 상단 제약조건을 따로 변수로 선언
     private var sheetControllTopConstraint: NSLayoutConstraint!
     
     
@@ -49,19 +53,18 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
     
     
     // Bottom Sheet과 safe Area Top 사이의 최소값을 지정하기 위한 프로퍼티
-    // 기본값은 30으로 지정
+    // 기본값은 0으로 지정해놓고 viewdidLoad시 초기화 해줌
     // 이 값이 최대 확장 시 화면 상단으로부터의 마진 값을 정해줌
-    var sheetPanMinTopConstant: CGFloat = 20.0
-    
+    var sheetPanMinTopConstant: CGFloat = 0.0
     
     
     // 열린 BottomSheet의 기본 높이를 지정하기 위한 프로퍼티
     // 이 값은 중간 크기 상황의 시트의 화면 상단으로부터의 마진 값을 정해줌
-    var defaultHeight: CGFloat = 450
+    var defaultHeight: CGFloat = 0.0
     
     // Bottom Sheet과 safe Area Bottom 사이의 최소값을 지정하기 위한 프로퍼티
     // 이 값이 시트 최소 축소 시 화면 하단으로부터의 마진 값을 정해줌
-    var sheetPanMinBottomConstant: CGFloat = 100.0
+    var sheetPanMinBottomConstant: CGFloat = 0.0
     
     
     override func viewDidLoad() {
@@ -69,7 +72,13 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
         
         self.view.backgroundColor = .gray
         
-
+        
+        //시트 확장, 기본, 최소 상태일 때 시트뷰 탑 제약조건 지정해줌
+        sheetPanMinTopConstant = sheetControll?.minTopConstant.rawValue ?? 20.0
+        
+        defaultHeight = sheetControll?.defaulTopConstant.rawValue ?? 450.0
+        
+        sheetPanMinBottomConstant = sheetControll?.minBottomConstant.rawValue ?? 100.0
         
         
         setTestBtn()
@@ -77,6 +86,9 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
         self.view.addSubview(testBtn)
         
         sheetControll = AMHomeSheetControll()
+        
+        
+        //바텀시트 뷰 관리를 AMHomeViewController에게 위임
         sheetControll?.actionDelegate = self
         
         contentSheetItemView = AMHomeContentSheetItemView()
@@ -93,8 +105,8 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
         viewPan.delaysTouchesBegan = false
         viewPan.delaysTouchesEnded = false
         
-        //바텀시트 영역에만 제스처 인식 기능을 추가한다. 바텀시트 영역을 제외한 부분 제스처는 인식되지않아야해서
-        // !!!! 우선 드래그인디케이터 뷰만 제스처 인식 가능하도록 변경 !!!
+        //바텀시트의 헨들바가 있는 영역 dragIndicatorView가 제스처를 통해 시트의 높이를
+        //변경할 수 있도록 UIPanGestureRecognizer 붙여준다
         sheetControll.dragIndicatorView.addGestureRecognizer(viewPan)
         
         
@@ -112,7 +124,7 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
         
         
 
-        //스크롤 뷰 제스처 인식하기 위해서 AMHomeViewController 대리자로 위임
+        //스크롤 뷰 제스처 인식에 따른 프로세스 처리를 AMHomeViewController에 위임
         contentSheetItemView.delegate = self
         
         
@@ -125,8 +137,8 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
         
         sheetControll.translatesAutoresizingMaskIntoConstraints = false
         
-        //바텀시트 초기 높이값 설정
-//        let topConstant: CGFloat = 300
+        //바텀시트 초기 높이값 설정 화면 밖 최하단 밑으로 설정
+        // 후에 디폴트 값인 화면 중단쯤으로 조정될 때 밑에서 부터 올라오는 효과를 부여해줄 수 있음
         let topConstant = view.safeAreaInsets.bottom + view.safeAreaLayoutGuide.layoutFrame.height
 
         sheetControllTopConstraint = sheetControll.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstant)
@@ -156,35 +168,37 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
     }
     
     //뷰컨의 view가 나타날때 showBottomSheet 실행해서 바텀시트 올라오도록하기
+    //이전에는 sheetControllTopConstraint는 바텀시트가 화면밖(화면 최하단 및)에 있도록 설정되어있음
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         showBottomSheet(atState: .normal)
     }
     
     
-    
+    //바텀시트의 높이를 최대확장, 디폴트, 최대축소로 변경하는 메서드
     private func showBottomSheet(atState: SheetViewState) {
         
-        //현제 바텀시트 상태 푸적
+        //현제 바텀시트 상태 추적
         currentSheetState = atState
         
+        // 화면 중단에 걸쳐져 있는 상태
         if atState == .normal {
             let safeAreaHeight: CGFloat = view.safeAreaLayoutGuide.layoutFrame.height
             let bottomPadding: CGFloat = view.safeAreaInsets.bottom
             sheetControllTopConstraint.constant = (safeAreaHeight + bottomPadding) - defaultHeight
             
-            
+            //확장 상태가 아님
             isExpanded = false
             
             //최대 확장 상태
         } else if atState == .expanded{
             
             
-            
+    
             
             sheetControllTopConstraint.constant = sheetPanMinTopConstant
             
-            
+            //최대 확장 상태임 (스크롤뷰 오프셋이 0일 때 아래로 제스처 시 시트가 내려갈 수 있도록 하기 위해 해당 변수 사용
             isExpanded = true
             
         } else {
@@ -195,7 +209,7 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
             sheetControllTopConstraint.constant = (safeAreaHeight + bottomPadding) - defaultHeight
             
             
-            //최소 확장상태
+            //최소 확장상태 만들어 주는 로직 (viewPanned메소드에 노말상태에서 아래로 제스처 시 return 시키는 부분도 제거해야 적용됨)
 //            let safeAreaHeight: CGFloat = view.safeAreaLayoutGuide.layoutFrame.height
 //            let bottomPadding: CGFloat = view.safeAreaInsets.bottom
 //            
@@ -206,10 +220,10 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
         UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseIn, animations: {
                     
             //뷰컨의 뷰 딤드 처리
-            self.view.alpha = 0.7
+//            self.view.alpha = 0.7
             
             
-                    // 바텀시트가 스무스하게 올라오도록 해줌
+            // 바텀시트가 스무스하게 올라오도록 해줌
             self.view.layoutIfNeeded()
         }, completion: nil)
     }
@@ -235,72 +249,89 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
     @objc private func viewPanned(_ panGestureRecognizer: UIPanGestureRecognizer) {
         
 
-        
-        
+        //제스처의 속도 측정
         let velocity = panGestureRecognizer.velocity(in: view)
         
+        //지정된 뷰의 좌표계에서, 이 view의 새 위치를 식별하는 점
+        // x와 y에 따라 가로 세로, 부호에 따라 방향이 반환된다.
         let translation = panGestureRecognizer.translation(in: self.view)
         
         
         print(translation)
         
         switch panGestureRecognizer.state {
+            
+        //제스처가 시작할 때 상황 (화면 터치되어있음)
         case .began:
             
-            
+            //현재 화면 상단으로부터의 바텀시트 top 제약조건 저장해놓음
             sheetPanStartingTopConstant = sheetControllTopConstraint.constant
+            
+        // 드래그가 진행 중 (터치되어 있는 상태에서 손가락의 위치가 이동하고 있음)
         case .changed:
             
+            //바텀시트가 중단에 있는 상황에서 드래그를 아래로 향했음
             if currentSheetState == .normal && translation.y > 0 {
                 
                 
                 // 노말 상테에서 아래로 드래그하는 동작 무시
+                // 노말 상태에서 바텀시트는 더이상 작아지면 안됨 고로 제스처 인식해줄 필요 없음
                 return
             }
             
-            if sheetPanStartingTopConstant + translation.y > sheetPanMinTopConstant {
+            // 바텀시트의 제스처 이전 탑 제약조건에 드래그 통해 새 좌쵸가 될 지점까지의 거리값을 더했을 때 최대 확장시 top제약조건 보다 크다면 sheetControllTopConstraint의 제약조건에 그 더한값을 지정해줌
+            // sheetPanStartingTopConstant의 값은 한번의 제스처 동안에는 그 값이  변하지않음
+            if sheetPanStartingTopConstant + translation.y >
+                sheetPanMinTopConstant {
                 sheetControllTopConstraint.constant = sheetPanStartingTopConstant + translation.y
             }
             
-            
+        
+            //드래그가 종료됨. 제스처 이후 화면에서 손가락이 떼졌음
         case .ended:
             
-            //빠르게 스크롤시 최소 크기
+            //빠르게 스크롤시 바텀시트를 최소 크기로 바꿔주는 로직
 //            if velocity.y > 2000 {
-//                showBottomSheet(atState: .normal)
+//                showBottomSheet(atState: .minimum)
 //                return
 //            }
 //            
-            
+            //핸드폰 안전구역 높이
             let safeAreaHeight = view.safeAreaLayoutGuide.layoutFrame.height
+            
+            //안전구역의 바텀영역값 (일반적으로 노치 디자인 34pt, 일반디자인 0pt)
             let bottomPadding = view.safeAreaInsets.bottom
-            // 1
+            
+            //핸드폰 화면에서 앱의 화면이 차지할 수 있는 전체 영역에서 바텀시트의 화면상단으로부터의 기본 마진값을 뺌
             let defaultPadding = safeAreaHeight+bottomPadding - defaultHeight
             
-            // 2
+            //제스처를 통해 새로 지정된 좌표값이 담긴 sheetControllTopConstraint.constant가 바텀시트 최대 확장 시 위치, 기본값 위치, 핸드폰 최하단 위치 이 세 지점에서 어디에 가장 근접한지 구함
             let nearestValue = nearest(to: sheetControllTopConstraint.constant, inValues: [sheetPanMinTopConstant, defaultPadding, safeAreaHeight + bottomPadding])
             
+            //최대확장 지점에 가장 가까울 시
             if nearestValue == sheetPanMinTopConstant {
                 print("Bottom Sheet을 Expanded 상태로 변경")
                 
-                //확장되자마자 상황은 시트내 스크롤이 아예 안되어잇음
-
-                
-
-            
-                
+                //바텀시트 최대확장 상태로 변경해줌 (이때 높이변화는 끊기지 않고 스무스하게 일어남)
                 showBottomSheet(atState: .expanded)
+                
             } else if nearestValue == defaultPadding {
+                
                 // Bottom Sheet을 .normal 상태로 보여주기
                 showBottomSheet(atState: .normal)
+                
+                
             } else {
+                
                 // Bottom Sheet을 숨기고 현재 View Controller를 dismiss시키기
+                //아래 메소드에 바텀시트를 아예 화면 밖으로 보내는 로직을 추가할 수도 있음
 //                hideBottomSheetAndGoBack()
                 
-                
+                //아니면 최소 상태로 변경 제스처 속도가 빠를때만 hideBottomSheetAndGoBack시키도록 분기할수도 있음
                 //최소 크기로 축소 (현재 제외)
                 //showBottomSheet(atState: .minimum)
                 
+                //현재는 디폴트 크기 normal과 최대확장 두가지로만 분기
                 showBottomSheet(atState: .normal)
             }
             
@@ -313,22 +344,21 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
     
     
     // 해당 메소드는 사용자가 view를 드래그하면 실행됨
-    
+    // 이 메소느는 UIScrollView인 AMHomeContentSheetItemView에 붙어있는 UIPanGestureRecognizer에 의해 호출됨
     // 오프셋이 0인경우 스크롤뷰를 아래로 내릴 시 시트가 내려가지도록 하기 위해서 구현함
     @objc private func viewPanned2(_ panGestureRecognizer: UIPanGestureRecognizer) {
-        
+
+        // 스크롤뷰의 오프셋이 0인 상황(컨텐트가 아래로 조금도 내려가있지않는 상태)에서 최대 확장시에만 시트 변화를 야기시키는 viewPanned2매소드를 진행시켜야함
         if(contentSheetItemView!.contentOffset.y <= 0 && isExpanded){
             
             print("that's it")
         }
         else{
+            // 그게 아니라면 스크롤뷰의 제스처 인식 기능만 활성화하고 시트뷰의 높이를 변화시켜주는 UIPanGestureRecognizer는 무시되어야함
             return
         }
         
         
-        print(" \n\n\n \(sheetControllTopConstraint.constant)")
-        
-
         
         
         let velocity = panGestureRecognizer.velocity(in: view)
@@ -366,7 +396,7 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
             let nearestValue = nearest(to: sheetControllTopConstraint.constant, inValues: [sheetPanMinTopConstant, defaultPadding, safeAreaHeight + bottomPadding])
             
             if nearestValue == sheetPanMinTopConstant {
-                print(" 222 222222  Bottom Sheet을 Expanded 상태로 변경")
+                print(" 2 Bottom Sheet을 Expanded 상태로 변경")
                 
                 //확장되자마자 상황은 시트내 스크롤이 아예 안되어잇음
 
@@ -388,30 +418,31 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
                 showBottomSheet(atState: .normal)
             }
             
-            print(" 2222222  222   드래그가 끝남")
+            print(" 2 드래그가 끝남")
         default:
             break
         }
-        print(" 22222222 222 유저가 위아래로 \(translation.y)만큼 드래그.")
+        print(" 2 유저가 위아래로 \(translation.y)만큼 드래그.")
     }
-    
     
     
     
     //주어진 CGFloat 배열의 값 중 number로 주어진 값과 가까운 값을 찾아내는 메소드
-    func nearest(to number: CGFloat, inValues values: [CGFloat]) -> CGFloat {
+    //배열을 순회화면서, 2개의 원소를 순차적으로 비교하면서 number와 차이가 가장 적은 원소가 계속해서 비교군이된다. 차이가 같은 원소가 있을 경우 인덱스가 더 낮은 원소가 선정된다.
+    func nearest(to number: CGFloat, inValues values: [CGFloat])->CGFloat {
         guard let nearestVal = values.min(by: { abs(number - $0) < abs(number - $1) })
         else { return number }
         return nearestVal
     }
+
     
-    
+    //해당 뷰컨트롤레 뷰에 붙어있는 플러스 버튼의 터치 인식에 의해 호출되는 메소드
     @objc func buttonTapped() {
         print("btn Tapped")
     }
     
     
-    
+    //AMHomeSheetControllDelegate를 채택하기 위해 구현한 메소드
     func didScroll() {
         print("did Scoll !!")
     }
@@ -419,10 +450,11 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
     
 
     
-    //contentSheetItem 스크롤 인식하는 메소드
+    //contentSheetItem 스크롤 인식하는 메소드 현재 해당 스크롤뷰에는 기본적으로 붙어있는 스크롤뷰 제스처 인식 기능기와(scrollViewDidScroll 메소드 등을 호출하는) UIPanGestureRecognizer의 제스처 인식기가 모두 붙어있다.
+    
+    //스크롤뷰 안 영역에 제스처를 수행했을 때 호출되는 메소드
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        // 스크롤 뷰의 contentOffset이 0이고, 사용자가 아래로 스크롤할 때
-        // 바텀 시트 상태를 .normal로 변경
+
         
 
         
@@ -432,36 +464,21 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
             
             
             
-            
-
-            
-//            print(" test : \(scrollView.bounces)")
             scrollView.bounces = true
         
-            //드래그 방향이 위로 향하는 순간 false로 처리해줘야함
+        //드래그 방향이 위로 향하는 순간 false로 처리해줘야함
         } else {
             // 상단의 경우 튕기지 않아야함
             scrollView.bounces = false
         }
         
-        //제스처 위에서 아래로 (위로 스크롤 중)
-        if scrollView.panGestureRecognizer.translation(in: scrollView).y > 0 {
-            
 
-            
-
-        }
-        
-        if scrollView.contentOffset.y <= 0 {
-
-        }
-        
-
+    
 
     }
     
 
-    
+    //스크롤뷰 안 영역에 드래그가 시작될 타이밍, 터치가 되어있을 때 호출되는 메소드
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         
             //드래그 시작 타이밍 떄마다 초기화 안해주면 빠르게 위아래 제스처시 상단도 튕길수도 있음
@@ -470,11 +487,7 @@ class AMHomeViewController: UIViewController,  AMHomeSheetControllDelegate, UISc
         
     }
     
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        
-
-        
-    }
+    
     
 }
 
